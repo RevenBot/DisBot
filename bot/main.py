906abfb229fc2fadf35ab7e7529c180fa9600023
Revenge <kevinjss97@gmail.com>
2021-05-08 19:50:22 +0200

    1.0.4

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,5 @@
 import discord
-import logging
 import os
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 token=os.get.env("DISCORD_BOT_TOKEN")
 
 class MyClient(discord.Client):
